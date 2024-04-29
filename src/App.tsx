deb091f8bad6a70c7e7561869b2b5809de629a30
shruti bera <shruti11.eie@gmail.com>
2024-04-29 08:09:48 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Message from './components/Message';
+function App() {
+  return (
+    <>
+<Message/>
+    </>
+  )
+}
+
+export default App
+
