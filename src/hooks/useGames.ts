38586074bbb80cd3470b409955ff235029220308
shruti bera <shruti11.eie@gmail.com>
2024-04-30 11:35:34 -0700

    Building Game cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react";
 import apiClientKey from "../services/api-client-key";
 import { CanceledError } from "axios";
 
-interface Game {
+export interface Game {
     id: number;
     name: string;
+    background_image: string;
 }
 
 interface FetchGamesResponse {
