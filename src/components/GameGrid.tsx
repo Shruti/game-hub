4b3b55651bc79ece11d77a70af337221e367239c
shruti bera <shruti11.eie@gmail.com>
2024-05-07 13:57:45 -0700

    Filter games by genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,12 +3,15 @@ import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
+import { Genre } from "../hooks/useGenres";
+interface Props {
+    selectedGenre: Genre | null ;
+}
 
-const GameGrid = () => {
-   const {data, error, isLoading} = useGames();
-   const skeletons = [1, 2, 3, 4, 5, 6];
-    return (
-      
+const GameGrid = ({selectedGenre} : Props) => {
+   const {data, error, isLoading} = useGames(selectedGenre);
+   const skeletons = [1, 2, 3, 4, 5];
+    return (      
          <SimpleGrid columns = {{sm: 1, md: 2, lg:3, xl: 5}} spacing={4} padding={10}>
             {error && <Text>{error}</Text>}
             {isLoading && 
@@ -18,7 +21,7 @@ const GameGrid = () => {
             </GameCardContainer>
             )}
            
-                {data.map ((game)=> (
+                {data && data.map ((game)=> (
                     <GameCardContainer key={game.id}>
                     <GameCard game={game}/>
                     </GameCardContainer>
@@ -29,4 +32,4 @@ const GameGrid = () => {
     )
 }
 
-export default GameGrid
+export default GameGrid;
